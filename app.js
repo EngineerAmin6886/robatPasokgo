e409fcf7698ace772a4d713918280fbee8087fad
EngineerAmin6886 <127695649+EngineerAmin6886@users.noreply.github.com>
2023-07-12 23:38:47 +0330

    farsi kon
    ezafh kardn farsi kon

---
+++ app.js
@@ -0,0 +1,21 @@
+function robatPasokgo(robat) {
+    if (robat === "سلام") {
+      console.log("سلام ");
+  
+    } else if (robat === "چه خبرا؟") {
+      console.log("سلامتی وجودت ");
+  
+    } else if (robat === "خوبی") {
+      console.log("قربانت ");
+  
+    } else if (robat === "چند سالته") {
+      console.log("30 ");
+      
+    } else if (robat === "سراغ اقای خاطری هم داری") {
+      console.log("نه حقیقت فقط میدونم روز های یکشنبه و سه شنبه میره آموزشگاه کلیک ");
+  
+    }else{
+      console.log('به دلیل حفظ اطلاعات شخصی دیگران معذوریم به این سوال شما پاسخ دهیم')
+    }
+  }
+  robatPasokgo("");
